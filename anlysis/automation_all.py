98f12e5b6bdb772d2b03516adbfe2584f1e3d363
chenbla <49671625+chenbla@users.noreply.github.com>
2022-03-13 22:29:44 +0200

    Update automation_all.py

--- anlysis/automation_all.py
+++ anlysis/automation_all.py
@@ -6,54 +6,91 @@ def run_command(command, workdir):
     for line in p.stdout:
         print(line.decode("utf-8"))#.replace('\n', ''))
 
-def run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight):
+def run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay):
     arguments = "--root my_images --g-model g_tnrd --d-model d_tnrd --model-config \"{'gen_blocks':3, 'dis_blocks':4, 'in_channels':1}\" --reconstruction-weight 1.0 --perceptual-weight 0 --adversarial-weight 0 --tnrd-energy-weight 0 --crop-size 100 --gray-scale --noise-sigma " + str(sigma) + " --epochs " + str(
         epocs) + " --step-size 150 --batch-size 2 --eval-every 10 --print-every 100 --lr " + str(lr) + " --device cuda --use-tb --results-dir " + output_dir + " --train_max_size " + str(train_max_size) + " --high_frequency_energy_weight " + str(high_frequency_energy_weight)
     if use_greedy_training:
         arguments + " --use_greedy_training "
+    if use_dct_drop_out:
+        arguments + " --use_dct_drop_out "
+    if dont_use_dct:
+        arguments + " --dont_use_dct "
+    if dct_weight_decay:
+        arguments + " --dct_weight_decay "
     command = "python main.py " + arguments
     run_command(command, code_folder)
 
-
+sigma_sizes = [25, 15, 50]
 train_sizes = [25, 50, 75, 125, 200, 300, 400]
-sigma_sizes = [15, 25, 50]
+epocs = 700
+lr = 1e-3
+high_frequency_energy_weight = 0
+use_greedy_training = False
+use_dct_drop_out = False
+dont_use_dct = False
+dct_weight_decay = False
 
 for sigma in sigma_sizes:
-    # train without greedy training
+    # original
     for train_size in train_sizes:
-        high_frequency_energy_weight = 0
-        use_greedy_training = False
-        #sigma = 50
-        epocs = 500
-        lr = 1e-3
         train_max_size = train_size
         label = "original_sigma={}_epocs={}".format(sigma, epocs)
         output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
         code_folder = os.path.dirname(os.path.dirname(__file__)) #"C:/project/denoising_my/"
-        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight)
-    #
-    # # train with greedy training
-    # for train_size in train_sizes:
-    #     use_greedy_training = True
-    #     sigma = 50
-    #     epocs = 2000
-    #     lr = 1e-3
-    #     train_max_size = train_size
-    #     label = "original-greedy_sigma={}_epocs={}".format(sigma, epocs)
-    #     output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
-    #     code_folder = os.path.dirname(os.path.dirname(__file__)) #"C:/project/denoising_my/"
-    #     run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training)
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
 
 
     # train with high frequency energy loss
     for train_size in train_sizes:
         high_frequency_energy_weight=1
-        use_greedy_training = False
-        #sigma = 50
-        epocs = 500
-        lr = 1e-3
+
         train_max_size = train_size
-        label = "hish-frequency-loss_sigma={}_epocs={}_high_frequency_energy_weight={}".format(sigma, epocs, high_frequency_energy_weight)
+        label = "high-frequency-loss_sigma={}_epocs={}_high_frequency_energy_weight={}".format(sigma, epocs, high_frequency_energy_weight)
         output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
         code_folder = os.path.dirname(os.path.dirname(__file__))
-        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight)
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training, high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
+
+    # greedy training
+    for train_size in train_sizes:
+        use_greedy_training = True
+
+        train_max_size = train_size
+        label = "greedy-training_sigma={}_epocs={}".format(sigma, epocs)
+        output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
+        code_folder = os.path.dirname(os.path.dirname(__file__))
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training,
+                      high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
+
+    # dct drop out
+    for train_size in train_sizes:
+        use_dct_drop_out = True
+
+        train_max_size = train_size
+        label = "dct-drop-out_sigma={}_epocs={}_dct_drop_out".format(sigma, epocs)
+        output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
+        code_folder = os.path.dirname(os.path.dirname(__file__))
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training,
+                      high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
+
+    # dont_use_dct
+    for train_size in train_sizes:
+        dont_use_dct = True
+
+        train_max_size = train_size
+        label = "dont-use-dct_sigma={}_epocs={}_dct_drop_out".format(sigma, epocs)
+        output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
+        code_folder = os.path.dirname(os.path.dirname(__file__))
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training,
+                      high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
+
+    # dct_weight_decay
+    for train_size in train_sizes:
+        dct_weight_decay = True
+
+        train_max_size = train_size
+        label = "dct-weight-decay_sigma={}_epocs={}_dct_drop_out".format(sigma, epocs)
+        output_dir = "./results/results_automation/output_dir_{}_samples_{}".format(train_max_size, label)
+        code_folder = os.path.dirname(os.path.dirname(__file__))
+        run_denoising(code_folder, sigma, epocs, lr, output_dir, train_max_size, use_greedy_training,
+                      high_frequency_energy_weight, use_dct_drop_out, dont_use_dct, dct_weight_decay)
+
